4fa0231ee1815a2d466f2f2484f589ca931e52c7
ahmed <ahmadabdullah4919@gmail.com>
2023-06-14 14:58:08 +0500

    ⚡️ removed orm completely

--- core/database.py
+++ core/database.py
@@ -1,12 +1,9 @@
-from sqlmodel import create_engine
 from pyodbc import connect
 
 from .configs import Settings
 
-cxn_str = f"mssql+pyodbc://{Settings.DB_SERVER}/{Settings.DB_NAME}?driver={Settings.DB_DRIVER}"
+cxn_str = f"DRIVER={Settings.DB_DRIVER};SERVER={Settings.DB_SERVER};DATABASE={Settings.DB_NAME};Trusted_Connection=yes;"
 
-pd_cxn_str = f"DRIVER={Settings.DB_DRIVER};SERVER={Settings.DB_SERVER};DATABASE={Settings.DB_NAME};Trusted_Connection=yes;"
-pd_connection = connect(pd_cxn_str)
-cursor = pd_connection.cursor()
+connection = connect(cxn_str)
 
-engine = create_engine(cxn_str, echo=False)
+cursor = connection.cursor()
